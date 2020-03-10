1e45520caea6e89679031be86f2dd44188182f58
JohnShaft <61475374+G-shaft@users.noreply.github.com>
2020-03-10 23:29:33 +0400

    Update g.py

--- g.py
+++ g.py
@@ -11,6 +11,6 @@ status 	= gpioObj.setInputDirection()
 loop = 1
 while loop == 1:
 	value = gpioObj.getValue()
-	print 'GPIO%d input value: %d'%(gpioNum, int(value))
+	print ('GPIO%d input value: %d'%(gpioNum, int(value)))
 	
 	time.sleep(1)
